7aa1c2a4c57b83d74e27e023c6c153401e12d5c2
EduHolg <eduholg@hotmail.com>
2024-04-11 18:42:12 -0500

    first commit

--- src/tt_um_FDC_chip.v
+++ src/tt_um_FDC_chip.v
@@ -17,16 +17,19 @@ module tt_um_FDC_chip (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-    assign uio_oe = 8'b0000_0000;
-    assign uio_out = 8'b0000_0000;
-    assign uo_out[7:5] = 3'b000;
+  //  assign uio_oe = 8'b0000_0000;
+  //  assign uio_out = 8'b0000_0000;
+  //  assign uo_out[7:5] = 3'b000;
 
-chip chip(
-	.reset(ui_in[3]),
-	.selec(ui_in[0]),
-	.clk_ref(ui_in[1]),
-	.VCO(ui_in[2]),
-	.out(uo_out{4:0})
-		);
-
+//chip chip(
+//	.reset(ui_in[3]),
+//	.selec(ui_in[0]),
+//	.clk_ref(ui_in[1]),
+//	.VCO(ui_in[2]),
+//	.out(uo_out{4:0})
+//		);
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+  
 endmodule
