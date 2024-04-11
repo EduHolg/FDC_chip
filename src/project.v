0deba9c49c6a94fdae46577c45400de608fd8f93
EduHolg <eduholg@hotmail.com>
2024-04-11 16:53:27 -0500

    first commit

--- src/project.v
+++ src/project.v
@@ -17,11 +17,9 @@ module tt_um_FDC_chip (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_oe  = 8'b0000_0000;
-  assign uio_out = 8'b0000_0000;
-  assign uio_in  = 8'b0000_0000;
- 
+    assign uio_oe = 8'b0000_0000;
+    assign uio_out = 8'b0000_0000;
+    assign uo_out[7:5] = 5'b0_0000;
 
 chip FDC_chip(
 .reset(ui_in[3]),
